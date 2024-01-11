b85243197d7351b3aa5feb8899193103d16f5f68
anshulkumar108 <anshulme96@gmail.com>
2024-01-11 21:38:55 +0530

    igniting our app

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,29 @@
 #Namaste React 
+
+
+# parcel
+dev build
+local server
+HMR=hot module replacement
+file watching alorithm-written in c++
+caching-Faster build
+image optimization
+Minification
+bundling
+compress
+consistent hashing
+code splitting
+differrential bundling-support old versions
+diagnostic
+Error handling
+to host app in HTTPS
+tree shaking-remove unsed code 
+
+   // "start": "rm -rf .parcel-cache && parcel && rm -rf dist",
+    "build": "rm -rf .parcel-cache && parcel build && rm -rf dist"
+
+    const heading=React.createElement("h1",{id:"heading"},"Namaste React🎉🎉🎯🎯");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-/* <div id="parent">
-    <div id="child1">
-        <h1>i am child element of parent tag</h1>
-    </div>
-     <div id="child2">
-        <h1>i am child element of parent tag</h1>
-    </div>
-</div> 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactElement(javascript object)=>HTML(Browser Understand)
-
-*/
-
-
-
-// # to insert only one tag
+// Create a React element for the heading
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "hello first code of react!"
+  "Hello first code of React!"
 );
 console.log(heading);
 
-// #if we to create parent and child relation between div .TO add sibling we to create array of third argument
-
-const parent=React.createElement("div",{id:"parent"},[
+// Create a React element for the parent div and its children
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
     React.createElement(
-        "div",
-        {id:"child1"},
-        [React.createElement("h1",{},"i am child1 element of h1 tag"),
-        React.createElement("h2",{},"i am child1 element of h2 tag")]
-    ), 
-     React.createElement(
-        "div",
-        {id:"child2"},
-        [React.createElement("h1",{},"i am child2 element of h1 tag"),
-        React.createElement("h2",{},"i am child2 element of h2 tag")]
+      "div",
+      { id: "child1" },
+      [
+        React.createElement("h1", {}, "I am child1 element of h1 tag 👩‍✈️"),
+        React.createElement("h2", {}, "I am child1 element of h2 tag")
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child2" },
+      [
+        React.createElement("h1", {}, "I am child2 element of h1 tag"),
+        React.createElement("h2", {}, "I am child2 element of h2 tag")
+      ]
     )
-]
-    );
+  ]
+);
 
+// Use ReactDOM to render the parent element into the root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(root);
 root.render(parent);
