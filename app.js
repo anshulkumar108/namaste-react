e4354671d1037a7c4deaf09f1b6ee823660fb273
anshulkumar108 <anshulme96@gmail.com>
2024-01-13 22:16:49 +0530

    laying the foundation

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Create a React element for the heading
+//React element
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello first code of React!"
+  "Namaste React🎉🎉🎯🎯"
 );
 console.log(heading);
 
-// Create a React element for the parent div and its children
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement(
-      "div",
-      { id: "child1" },
-      [
-        React.createElement("h1", {}, "I am child1 element of h1 tag 👩‍✈️"),
-        React.createElement("h2", {}, "I am child1 element of h2 tag")
-      ]
-    ),
-    React.createElement(
-      "div",
-      { id: "child2" },
-      [
-        React.createElement("h1", {}, "I am child2 element of h1 tag"),
-        React.createElement("h2", {}, "I am child2 element of h2 tag")
-      ]
-    )
-  ]
-);
+//JSX -HTML LIKE 0R XML LIKE
+//jsx is syntax not react element.react element is an object
+//jsx code is transpiled before it reaches the js engine and transpiling is done by parcel.parcel act as manager.parcel has its packages .for transpiling the code parcel give responsbility to babel.
+//babel is javascript compiler and its a library
+//JSX transpiled into(with help of babel) => React.createElement-Js Object => HTMLElement(render)
 
-// Use ReactDOM to render the parent element into the root element
+const jsxheading = <h1>Namaste react using JSX ❤️</h1>; //react element using jsx
+console.log(jsxheading);
+
+//React component........there are two type of component
+//1.class based component-OLD WAY
+//2.function based component-NEW WAY:-ITS AN normal javascript function and always start with capital letter and it returns JSX CODE(react element)
+
+
+//js element 
+const  title= <h1>this is Anshul kumar </h1>
+
+const Title = () => {
+  return <h1 className="title">Namaste React🎉🎉🎯🎯</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title></Title>
+      <Title /> 
+      {Title()} //function calling inside jsx 
+      {title}
+      <h1>this is react functional component 🌲🌳🌴🍂🍁🪺🥑🎇🎇</h1>
+    </div>
+  );//this is component composition
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent);
+
+// root.render(jsxheading);
+
+//to render component
+root.render(<HeadingComponent />);
